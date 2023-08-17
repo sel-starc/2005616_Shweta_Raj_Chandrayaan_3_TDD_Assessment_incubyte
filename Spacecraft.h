3116c92812037518e533c704ef2cf39fc6432ace
sel-starc <90095060+sel-starc@users.noreply.github.com>
2023-08-17 15:40:09 +0530

    Add files via upload

--- Spacecraft.h
+++ Spacecraft.h
@@ -1,16 +1,26 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+#include <string>
 
 class Spacecraft {
     int x, y, z;
     char dir;
     char lastDir;
+
 public:
     Spacecraft(int startX, int startY, int startZ, char startDir);
-    void doCommand(char cmd);
-    void showPosition();
 
+    void doCommand(char cmd);
+    void move(int step);
+    void turn(char cmd);
     char leftTurn(char d);
     char rightTurn(char d);
+    void showPosition() const;
+
+  
+    int getX() const { return x; }
+    int getY() const { return y; }
+    int getZ() const { return z; }
+    char getDirection() const { return dir; }
 };
+
 
